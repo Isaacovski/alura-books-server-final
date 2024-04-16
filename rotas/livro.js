d6feb885fb35085cb94d03ac785f23407d372a74
Isaac <codebiisaac@gmail.com>
2024-04-15 22:36:41 -0300

    iniciando criação das rotas http get post path e delete

---
+++ rotas/livro.js
@@ -0,0 +1,15 @@
+// livro.js
+
+const { Router } = require("express")
+
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.send('Olá mundo da Alura do Brasil!')
+})
+
+router.post('/', (req, res) => {
+    res.send('Você fez uma requisição do tipo POST')
+})
+
+module.exports = router
